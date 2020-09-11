d23d631ba20e4dbe96b49735bfefabec820fca5d
Alexey Bubnov <bubnov.alexey@gmail.com>
2020-09-11 05:30:44 +0300

    40

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -5,9 +5,35 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * Class blogpost
+ * @package App\Models
+ *
+ * @property \App\Models\BlogCategory $category
+ * @property \App\Models\User $user
+ * @property string $title
+ * @property string $slug
+ * @property string $excerpt
+ * @property string $content_raw
+ * @property string $content_html
+ * @property string $published_at
+ * @property boolean $is_published
+ */
 class BlogPost extends Model
 {
     use SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'location_id',
+        'category_id',
+        'slug',
+        'excerpt',
+        'content_raw',
+        'content_html',
+        'is_published',
+        'user_id',
+    ];
 
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
